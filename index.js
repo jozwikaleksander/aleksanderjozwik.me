b1380509de1981f6e881838f6a89bb5df55ce16c
Aleksander Jóźwik <aleksanderjozwik@elektronik.edu.pl>
2023-05-19 15:37:47 +0200

    Fixing website responsivness

--- index.js
+++ index.js
@@ -1,43 +1,63 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
+// Height
+const height = 300;
+
+// Setting up the scene, camera, and renderer
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 5;
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / height, 0.1, 1000 );
+camera.position.z = 3;
 camera.position.y = 1;
 
-const speed = 0.008;
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true});
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, height );
+
+const container = document.getElementById( 'container' );
+container.appendChild( renderer.domElement );
+
 const clock = new THREE.Clock();
 
+// Speed of the rotation
+const speed = 0.008;
+
+// Texture loader
 const loader = new THREE.TextureLoader();
 
-const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true});
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+// Adding the orbit controls
+const controls = new OrbitControls( camera, renderer.domElement );
+scene.add(controls.object);
+controls.update();
 
-const sun_geometry = new THREE.IcosahedronGeometry( 0.5, 2 ); 
-const sun_mat = new THREE.MeshBasicMaterial( { map: loader.load('/sun.png') } );
-const sun = new THREE.Mesh( sun_geometry, sun_mat );
-const pointLight = new THREE.PointLight( 0xffffff, 1, 100 );
-pointLight.position.set( 0, 0, 0 );
-sun.add( pointLight );
+// Planet function
+const planet = (mat, size, x, y, z) => {
+    const geometry = new THREE.IcosahedronGeometry( size, 1 );
+    const material = new THREE.MeshBasicMaterial( mat );
+    const mesh = new THREE.Mesh( geometry, material );
+    mesh.position.x = x;
+    mesh.position.y = y;
+    mesh.position.z = z;
+    return mesh;
+}
 
+// Sun
+const sun = planet({ map: loader.load('/sun.png') }, 0.5, 0, 0, 0);
 scene.add(sun);
 
-const satelitte_geometry = new THREE.IcosahedronGeometry( 0.1,1 ); 
-const satelitte_mat = new THREE.MeshBasicMaterial( { map: loader.load('/planet.png') } );
-const satelitte = new THREE.Mesh( satelitte_geometry, satelitte_mat );
-
+// Earth
+const satelitte = planet({ map: loader.load('/planet.png') }, 0.1, 0, 0, 0);
 scene.add(satelitte);
 
-const circle = new THREE.CircleGeometry( 2, 48 );
-const circle_edges = new THREE.EdgesGeometry( circle );
-const circle_mesh = new THREE.LineSegments( circle_edges, new THREE.LineBasicMaterial( { color: 0xcdd6f4 } ) );
-circle_mesh.rotation.x = Math.PI/2;
-scene.add( circle_mesh );
+// Trajectory
+const circle = new THREE.LineSegments(  new THREE.EdgesGeometry( new THREE.CircleGeometry( 2, 48 ) ), new THREE.LineBasicMaterial( { color: 0xcdd6f4 } ) );
+circle.rotation.x = Math.PI/2;
+scene.add( circle );
 
 function animate() {
 	requestAnimationFrame( animate );
+
+    controls.update();
 
     var delta = clock.getDelta();
     var elapsed = clock.getElapsedTime();
